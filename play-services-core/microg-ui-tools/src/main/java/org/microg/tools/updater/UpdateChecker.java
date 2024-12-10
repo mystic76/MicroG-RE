ec9a824b4df1bf8b348fd839484ebf897bbd6d6f
mystic76 <contact.iamlynx@gmail.com>
2024-12-10 18:46:49 +0530

    Update UpdateChecker.java

--- play-services-core/microg-ui-tools/src/main/java/org/microg/tools/updater/UpdateChecker.java
+++ play-services-core/microg-ui-tools/src/main/java/org/microg/tools/updater/UpdateChecker.java
@@ -24,8 +24,8 @@ import okhttp3.ResponseBody;
 
 public class UpdateChecker {
 
-    private static final String GITHUB_API_URL = "https://api.github.com/repos/WSTxda/MicroG-RE/releases/latest";
-    private static final String GITHUB_RELEASE_LINK = "https://github.com/WSTxda/MicroG-RE/releases/latest";
+    private static final String GITHUB_API_URL = "https://api.github.com/repos/abhiram79/Micro-GMS-Core/releases/latest";
+    private static final String GITHUB_RELEASE_LINK = "https://github.com/abhiram79/Micro-GMS-Core/releases/latest";
     private static final String ERROR_NO_RESPONSE = "ERROR_NO_RESPONSE";
 
     private final WeakReference<Context> contextRef;
